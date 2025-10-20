9a96c917ce278825c2e974749c4cdad4033b48ae
Hodow-dev <felipeoliveratom@gmail.com>
2025-10-20 10:15:28 -0500

    Add files via upload

---
+++ luna.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const luna = document.querySelector(".luna");
+  let posY = 0;
+  let targetY = 0;
+
+  function animar() {
+    posY += (targetY - posY) * 0.05;
+    luna.style.transform = `translateY(${posY * 0.8}px)`; // antes 0.3 → ahora 0.8
+    requestAnimationFrame(animar);
+  }
+
+  window.addEventListener("scroll", () => {
+    targetY = window.scrollY;
+  });
+
+  animar();
+});
